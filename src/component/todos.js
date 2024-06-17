4fdaceadafe02e186b75e36214fa44e96c9e96de
Aliya Tang <aliyatang11@gmail.com>
2024-06-17 15:38:10 -0400

    fixed small error

--- src/component/todos.js
+++ src/component/todos.js
@@ -21,7 +21,7 @@ const Todos = ({ todos, deleteTodo }) => {
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} */}
             <ListItemButton component="a" href="#simple-list" style={{marginTop:10}}>
-              <Checkbox style={{ paddingLeft: 0 }} color={primary} onClick={() => deleteTodo(todo.id)} />
+              <Checkbox style={{ paddingLeft: 0 }} color="primary" onClick={() => deleteTodo(todo.id)} />
               <ListItemText primary={todo.content} secondary ={todo.date}/>
             </ListItemButton>
           </Card>
